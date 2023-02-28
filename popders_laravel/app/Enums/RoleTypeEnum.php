2396bcb324176b6c23b0a83ae19ee86c50b3c2ab
123meena_github <habibimina5@gmail.com>
2023-02-28 15:08:16 +0100

    new commit

---
+++ popders_laravel/app/Enums/RoleTypeEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum RoleTypeEnum:int{
+    case Admin = 1;
+    case Coder = 2;
+}
--- popders_laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ popders_laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('role');//recibe numeros segun cantidad de roles, a definir en el modelo
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
