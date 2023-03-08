a0fa871c19df73858789b9fd7f39f378d1be8017
SharonInfante <sharonshabel@gmail.com>
2023-03-08 09:27:53 +0100

    update

--- popders_laravel/app/Http/Controllers/SongController.php
+++ popders_laravel/app/Http/Controllers/SongController.php
@@ -39,25 +39,26 @@ class SongController extends Controller
         return view('songDescription', compact('songs'));
     }
 
-    public function edit(Song $songs) 
+    public function edit($id_song) 
     {   
+        $songs = Song::find($id_song); 
         return view('editSong', compact('songs'));
     }
 
-    // public function update(Request $request, Song $song)
-    // {
-    //     $songs = new Song();
+    public function update(Request $request, $id_song)
+    {
+        $songs = new Song($id_song);
 
-    //     $songs->title = $request->title;
-    //     $songs->artist = $request->artist;
-    //     $songs->genre = $request->genre;
-    //     $songs->url = $request->url;
-    //     $songs->image = $request->image;
+        $songs->title = $request->title;
+        $songs->artist = $request->artist;
+        $songs->genre = $request->genre;
+        $songs->url = $request->url;
+        $songs->image = $request->image;
 
-    //     $songs->save();
+        $songs->save();
 
-    //     return redirect()->route('songDescription');
-    // }
+        return redirect()->route('songDescription.show', $songs);
+    }
 
     
 }
