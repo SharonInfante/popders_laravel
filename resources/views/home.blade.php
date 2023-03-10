f6ed4ff41495bc5967996546e1dc0c8f5030ae99
DianaB177 <anaidb77@gmail.com>
2023-03-10 20:39:14 +0100

    limpieza de archivo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+
+@section('content')
+
+<a href="{{route('login')}}">Longin</a>
+<a href="{{route('register')}}">Register</a>
+
+<h1>POPDERS HOME</h1>
+
+<a href="{{route('playlist.index')}}">Playlist</a>
+
+@endsection
