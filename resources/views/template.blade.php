fbcd9d9b84230f078968e0984c1a73b6ae1ad42d
DianaB177 <anaidb77@gmail.com>
2023-03-10 20:49:10 +0100

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,12 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href='../../../css/app.css'>
-    <title>Document</title>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    @vite('resources/css/app.css')
+    <style>
+        .img{
+            background-image: url("{{asset('img/backimg.jpg')}}");
+        }
+    </style>
+    <title>@yield('title')</title>
+    <!-- favicon -->
+    <!-- estilos -->
 </head>
-<body>
+<body class="img bg-no-repeat bg-cover bg-center">
     
-
+    <!-- header -->
+    <!-- nav -->
     @yield('content')
+    <!-- footer -->
+    <!-- script -->
+
 </body>
 </html>
